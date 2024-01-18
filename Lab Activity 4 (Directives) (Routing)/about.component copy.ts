037164d8cad115ed05ebd554ffe87930f95601a1
chadchad0555 <123538151+chadchad0555@users.noreply.github.com>
2024-01-18 10:07:50 +0800

    Add files via upload

---
+++ Lab Activity 4 (Directives) (Routing)/about.component copy.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  imageUrl:string="assets/profile.png"; 
+imageW: number = 210;
+  
+
+}
